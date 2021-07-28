5dbcd3135297775827e88b6eeab9584e1552ca08
sharmadi <divyue.sharma@viacomcontractor.com>
2021-07-28 19:28:32 +0530

    template-driven-form

--- src/app/template-driven-forms-contact/template-driven-forms-contact.component.ts
+++ src/app/template-driven-forms-contact/template-driven-forms-contact.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenFormsContactComponent implements OnInit {
 
+  onLog(x){
+    console.log(x);
+  }
   constructor() { }
 
   ngOnInit(): void {
